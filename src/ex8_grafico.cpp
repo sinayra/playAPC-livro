36d993568a5d0ca6cea3929e73f0def38e457cd5
sinayra <nayra@hotmail.co.jp>
2016-03-11 11:40:50 -0300

    Capítulo 2

---
+++ src/ex8_grafico.cpp
@@ -0,0 +1,25 @@
+#include <playCB/playcb.h>
+#include <math.h>
+
+int main(){
+    Ponto p[100];
+    int i, j;
+
+    MudaLimitesJanela(125000);
+
+    AbreJanela(400, 400, "Criando um grafico");
+    PintarFundo(255, 255, 255);
+    MostraPlanoCartesiano(7000);
+
+    j = -50;
+    for(i = 0; i < 100; i++, j++){
+        p[i].x = j;
+        p[i].y = -pow(p[i].x, 3);
+    }
+
+    CriaGrafico(100, p, 1);
+    Pintar(255, 0, 0);
+
+    Desenha();
+
+}
